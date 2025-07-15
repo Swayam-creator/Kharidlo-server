65b53fc344b5357f630bdf7b830f4c55ee1daa06
Swayam-creator <swayamallewar7@gmail.com>
2025-07-15 18:52:57 +0530

    redis url updated

--- src/config/reddis.js
+++ src/config/reddis.js
@@ -2,11 +2,5 @@ import Redis from "ioredis"
 import dotenv from 'dotenv';
 import path from 'path'
 dotenv.config({ path: path.resolve(process.cwd(), './src/config/.env') });
-if(!process.env.UPSTASH_REDIS_URI){
-    console.log(process.env.UPSTASH_REDIS_URI)
-    console.log(path.resolve(process.cwd(), './src/config/.env'))
-    throw new Error("Env variable UPSTASH_REDIS_URI not set");
-}
 export const redis = new Redis(process.env.UPSTASH_REDIS_URI);
-redis.on("error",(err)=>{console.log(err)});
-redis.on("connect",(err)=>{console.log("Redis connected successfully")});
+
--- src/controllers/analytics.controller.js
+++ src/controllers/analytics.controller.js
@@ -1,26 +1,82 @@
 import { Product } from "../models/product.model.js";
 import User from "../models/user.model.js"
+import {Order} from "../models/order.model.js"
 export const getAnalyticsData=async(req,res)=>{
     try {
         const totalUsers=await User.countDocument();
         const totalProduct=await Product.countDocument();
-        const salesDat=await db.aggregate([
+        const salesData=await Order.aggregate([
             {
                 $group:{
-                    _id:null,
+                    _id:null, // select all products 
                     sales:{$sum:1},
                     revenue:{$sum:"$totalPrice"}
                 },
             }
         ]);
-        const {sales,revenue}=salesDat() || {sales:0,revenue:0}
+        console.log(salesData);
+        const {sales,revenue}=salesData[0] || {sales:0,revenue:0}
         return {
-         users:totalUser,
-         sales:totalSales,
-         
+         users:totalUsers,
+         sales:sales,
+         revenue:revenue,
+         totalProducts:totalProduct
         }
 
     } catch (error) {
-        
+        return res.status(500).json({message:error.message,success:false});
     }
+}
+
+export const getDailyAnalyticsData=async(startDate,endDate)=>{
+    try {
+        const dailySalesData=await Order.aggregrate(
+        [{
+              $match:{
+                createdAt:{
+                    $gte:startDate,
+                    $lte:endDate,
+                },
+              },
+              $group:{
+                _id:{$dateToString:{ format: "%Y-%m-%d", date: "$createdAt" } },
+                sales:{$sum:1},
+                revenue:{$sum:"$totalPrice"}
+              },
+        },
+        {$sort:{_id:1} }
+        ]
+    );
+    
+//   {   
+//    _id : "15-7-2025"
+//    sales:123,
+//     revenue:123  
+//  }
+  
+    const dateArray=getDatesInRange(startDate,endDate);
+
+      return dateArray.map((date)=>{
+        const foundData=dailySalesData.find((item)=>item._id===date);
+        return {
+            date,
+            revenue:foundData?.revenue,
+            sales:foundData?.sales,
+        }
+      })
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message,success:false});
+    }
+}
+
+
+const getDatesInRange=function(startDate,endDate){
+    let dates=[];
+    const currentDate=new Date(startDate);
+    while(currentDate<=endDate){
+       dates.push(currentDate.toISOString().split('T')[0]);
+       currentDate=currentDate.set(currentDate.getDate()+1);
+    }
+  return dates;
 }
